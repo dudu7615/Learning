484bde3c2918747e5dd37611027cf2b8c12bd341
dudu7615 <dudu7615@qq.com>
2025-07-20 20:14:24 +0800

    chore: exclude node_modules from markdown file search in conversion script

--- .tools/md2html.sh
+++ .tools/md2html.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find . -type f -name '*.md' | while read mdfile; do
+find . -type f -name '*.md' -not -path './node_modules/*' | while read mdfile; do
     htmlfile="public/html/${mdfile#./}"
     htmlfile="${htmlfile%.md}.html"
     mkdir -p "$(dirname "$htmlfile")"
